aee211d94944e3655188e64ef41836081a8100f0
David Gevorgyan <dav.gevorgyan.main@gmail.com>
2025-10-28 19:17:32 +0400

    change: changeCaseStatus return response object

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -7,6 +7,7 @@ const userRouter = e.Router()
 
 userRouter.get("/all", authToken, checkRole(["admin"]), UserController.getAllUsers)
 userRouter.get("/:id", authToken, checkRole(["admin"]), UserController.getUserById)
+userRouter.get("/activation/:userId", UserController.getUserActivation)
 userRouter.delete("/:id", authToken, checkRole(["admin"]), UserController.deleteUser)
 userRouter.put("/:id", authToken, UserController.updateUser)
 
