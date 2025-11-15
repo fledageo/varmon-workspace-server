94038c6ba21b785445271100f4bbffef932aed4a
fledageo <dav.gevorgyan.main@gmail.com>
2025-11-15 22:13:00 +0400

    change: pachage.json and add new Frontend url

--- src/server.js
+++ src/server.js
@@ -1,39 +1,38 @@
-import env from "dotenv"
-import express from "express"
-import cors from "cors"
-import prisma from "../prisma/prismaClient.js"
-import router from "./routes/router.js"
-import cookieParser from "cookie-parser"
+import env from "dotenv";
+import express from "express";
+import cors from "cors";
+import prisma from "../prisma/prismaClient.js";
+import router from "./routes/router.js";
+import cookieParser from "cookie-parser";
 
-
-env.config()
-const app = express()
-const PORT = process.env.PORT || 5000
-
+env.config();
+const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.use(cookieParser());
-app.use(cors({
+app.use(
+  cors({
     origin: process.env.FRONTEND_URL,
-    credentials: true, 
-}))
-app.use(express.json())
-app.use("/api", router)
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use("/api", router);
 
 async function checkConnectionDb() {
-    try {
-        await prisma.$connect();
-        console.log('Database connected successfully!')
-    } catch (error) {
-        console.log("Database connection failed:", error)
-        process.exit(1)
-    }
+  try {
+    await prisma.$connect();
+    console.log("Database connected successfully!");
+  } catch (error) {
+    console.log("Database connection failed:", error);
+    process.exit(1);
+  }
 }
 
-checkConnectionDb()
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
-
+checkConnectionDb();
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 
 process.on("SIGINT", () => {
-    prisma.$disconnect();
-    process.exit(0)
-})
+  prisma.$disconnect();
+  process.exit(0);
+});
