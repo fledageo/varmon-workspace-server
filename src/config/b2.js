26f063ac0998b07ae4c55818a4a382334e94be27
karen <karengharagyozyan11@gmail.com>
2025-11-22 17:04:45 +0400

    init project

--- src/config/b2.js
+++ src/config/b2.js
@@ -1,8 +1,8 @@
 const { S3Client } = require("@aws-sdk/client-s3");
 
 const s3 = new S3Client({
-  region: "eu-central-003",
-  endpoint: "https://s3.eu-central-003.backblazeb2.com",
+  region: "us-east-005",
+  endpoint: process.env.B2_ENDPOINT,
   credentials: {
     accessKeyId: process.env.B2_KEY_ID,
     secretAccessKey: process.env.B2_APPLICATION_KEY,
